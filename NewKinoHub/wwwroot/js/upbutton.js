60d59acd7cb83a63e346d075e3bc4c925e24611d
Anton-1230 <72217575+Anton-1230@users.noreply.github.com>
2021-04-30 11:26:39 +0300

    Добавил кучу всякого полезного

---
+++ NewKinoHub/wwwroot/js/upbutton.js
@@ -0,0 +1,26 @@
+﻿ src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js">
+ 
+
+$(function() {
+ 
+$(window).scroll(function() {
+ 
+if($(this).scrollTop() != 0) {
+ 
+$('#toTop').fadeIn();
+ 
+} else {
+ 
+$('#toTop').fadeOut();
+ 
+}
+ 
+});
+ 
+$('#toTop').click(function() {
+ 
+$('body,html').animate({scrollTop:0},800);
+ 
+});
+ 
+});
