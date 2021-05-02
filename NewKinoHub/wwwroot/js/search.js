00e095337c16a9a14f863a4820cc4a401591a78d
Anton-1230 <72217575+Anton-1230@users.noreply.github.com>
2021-05-02 21:43:13 +0300

    Добавил ссылки на актеров Изменил странички регистрации и входа Изменил окно с жанрами в фильмах

---
+++ NewKinoHub/wwwroot/js/search.js
@@ -0,0 +1,9 @@
+﻿function checkParams() {
+    var search = $('#search').val();
+
+    if (search.length != 0) {
+        $('#submit').removeAttr('disabled');
+    } else {
+        $('#submit').attr('disabled');
+    }
+}
