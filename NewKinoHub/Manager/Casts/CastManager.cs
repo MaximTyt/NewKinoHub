6172d5ef96406e98f86f72a878ce23ff647d567e
Anton-1230 <72217575+Anton-1230@users.noreply.github.com>
2021-05-08 21:13:16 +0300

    изменил профиль

--- NewKinoHub/Manager/Casts/CastManager.cs
+++ NewKinoHub/Manager/Casts/CastManager.cs
@@ -18,7 +18,7 @@ namespace NewKinoHub.Manager.Casts
 
         public  async Task<ICollection<Cast>> GetAllCast(int castId)
         {
-            return await _context.Casts.Include(st => st.Person).Where(st=>st.Media.MediaID == castId && st.RoleInFilm == RoleInFilm.Actor).ToListAsync();
+            return await _context.Casts.Include(st => st.Person).Where(st=>st.Media.MediaID == castId && st.RoleInFilm == RoleInFilm.Актёр).ToListAsync();
         }
 
         public async Task<Cast> GetPersonforId(int personId)
