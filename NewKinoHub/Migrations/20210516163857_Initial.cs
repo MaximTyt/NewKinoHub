4f0ff303eb3082ba5c2514d9f979a952894f9ad3
Maxim Tyt <imaximtyt@gmail.com>
2021-05-17 21:11:44 +0300

    Изменил вывод возраста(теперь он меняется)

--- NewKinoHub/Migrations/20210516163857_Initial.cs
+++ NewKinoHub/Migrations/20210516163857_Initial.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace NewKinoHub.Migrations
 {
-    public partial class m1 : Migration
+    public partial class Initial : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
